1d2df07016b1850099acf1bd095e3785643bfe18
Mario Haefs <MarioHaefs@t-online.de>
2023-05-03 15:34:55 +0200

    added first models

--- js/game.js
+++ js/game.js
@@ -1,13 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
+let world = new World();
 
 
 function init() {
     canvas = document.getElementById('canvas');
     cts = canvas.getContext('2d');
 
-    character.src = '../assets/img/2_character_pepe/2_walk/W-21.png';
-
-    ctx.drawImage(character, 20, 20, 50, 150);
+    console.log('My Character is', world.character);
 }
