e665faa3c1395e72a5786503622a894c8233b5e9
Mario Haefs <MarioHaefs@t-online.de>
2023-05-12 12:04:36 +0200

    added small chicken

--- models/small-chicken.class.js
+++ models/small-chicken.class.js
@@ -1,11 +1,11 @@
-class Chicken extends MovableObject { 
+class SmallChicken extends MovableObject { 
     y = 355;
     height = 70;
     width = 100;
     IMAGES_WALKING = [
-        'assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        'assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        'assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+        'assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'assets/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'assets/img/3_enemies_chicken/chicken_small/1_walk/3_w.png', 
     ];
     
 
@@ -14,7 +14,7 @@ class Chicken extends MovableObject {
         this.loadImages(this.IMAGES_WALKING);
 
         this.x = 400 + Math.random() * 2000; // Zahl zwischen 200 und 700
-        this.speed = 0.15 + Math.random() * 1;
+        this.speed = 0.15 + Math.random() * 0.5;
 
         this.animate();
     }
